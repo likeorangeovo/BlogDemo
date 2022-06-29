5c21b6958d4a24d20d0e05c6e3b19c2d31d4f4b2
Your Name <you@example.com>
2022-06-29 11:52:35 +0800

    增加了文章图片随机展示功能

--- src/components/ArticleItem.vue
+++ src/components/ArticleItem.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container" @click="handleClick">
     <img
-      src="https://res.cloudinary.com/chengchengcheng/image/upload/v1615820198/blog/%E9%A6%96%E9%A1%B5/v2-7e99c72876b50237b72ca4f8a393cf55_720w_yjmjfx.jpg"
+      :src="this.showurl"
       alt=""
     />
     <div class="message">
@@ -41,6 +41,30 @@ export default {
   data() {
     return {
       tags: [],
+      showurl:'',
+      imgurl:[
+        'https://res.cloudinary.com/chengchengcheng/image/upload/v1615820198/blog/%E9%A6%96%E9%A1%B5/v2-7e99c72876b50237b72ca4f8a393cf55_720w_yjmjfx.jpg',
+        'https://res.cloudinary.com/chengchengcheng/image/upload/v1615819637/blog/%E9%A6%96%E9%A1%B5/v2-8d6aaa767ec5f0dad922bb3cf11b3847_r_k3cbcc.jpg',
+        'https://res.cloudinary.com/chengchengcheng/image/upload/v1615024800/blog/%E9%A6%96%E9%A1%B5/topm_vacxce.jpg',
+        'https://res.cloudinary.com/chengchengcheng/image/upload/v1623160276/bimage/v2-270b7e00013a57461cf6f1b6ddf846e6_720w_fzvqvi.jpg',
+        'https://res.cloudinary.com/chengchengcheng/image/upload/v1615031053/blog/v2-5f7cb7e900b9dcf5354c3d4d2c5cc3c2_720w_hydvll.jpg',
+        'https://res.cloudinary.com/chengchengcheng/image/upload/v1615031053/blog/v2-023dea2a5198795d4344deb9ee5bb020_720w_hjvnjh.jpg',
+        'https://res.cloudinary.com/chengchengcheng/image/upload/v1615030005/blog/v2-946a282c299f5b927eb5790a23b04de5_r_je3wgb.jpg',
+        'https://res.cloudinary.com/chengchengcheng/image/upload/v1615030005/blog/v2-37cd97b322e52240e2b85c6ff67edad2_r_o3dafb.jpg',
+        'https://res.cloudinary.com/chengchengcheng/image/upload/v1615030005/blog/topm1_ixbm7c.jpg',
+        'https://res.cloudinary.com/chengchengcheng/image/upload/v1615031054/blog/v2-963d6056156430c5dde501dafc37d1e2_720w_juwuyq.jpg',
+        'https://vip1.loli.io/2022/05/25/Nlgqm3wzAQPieX1.jpg',
+        'https://vip2.loli.io/2022/05/25/erTYAV4EwqZ62nx.jpg',
+        'https://vip1.loli.io/2022/05/25/Rv2d9m1li3qo5bz.jpg',
+        'https://vip1.loli.io/2022/05/15/H5UcQTNpvPtxEb3.jpg',
+        'https://vip2.loli.io/2022/05/15/HfXUVhxeNpvBwsq.jpg',
+        'https://vip2.loli.io/2022/05/15/JD9es1PX5am3FGr.jpg',
+        'https://vip2.loli.io/2022/05/15/pXnyuUx4o7qivHb.jpg',
+        'https://vip2.loli.io/2022/05/09/iCL9GxptQmJ5fB6.jpg',
+        'https://res.cloudinary.com/chengchengcheng/image/upload/v1655904319/blog/icecream_c6amf8.jpg',
+        'https://res.cloudinary.com/chengchengcheng/image/upload/v1615030006/blog/v2-bee41bb2de490aa839553cb3d0db0cfd_720w_hqkxd6.jpg',
+
+      ]
     };
   },
   props: ["artdata"],
@@ -104,6 +128,10 @@ export default {
       this.$emit("delete", this.artdata.id);
     },
   },
+  created(){
+    const num = Math.floor(Math.random()*20);
+    this.showurl = this.imgurl[num]
+  }
 };
 </script>
 

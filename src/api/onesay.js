c3bd9508488094a61d2375032a16c04b6e21ce1f
Your Name <you@example.com>
2022-06-29 17:10:32 +0800

    修改接口

--- src/api/onesay.js
+++ src/api/onesay.js
@@ -1,2 +1,2 @@
 import axios from "axios";
-export const Onesay = axios.get('https://v1.hitokoto.cn?c=i&c=h&c=j', { withCredentials: false})
+export const Onesay = axios.get('https://v1.hitokoto.cn?c=i&c=k', { withCredentials: false})

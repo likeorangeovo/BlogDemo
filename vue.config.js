f2d85282520d44548683abe4883588098396f2d4
likeorangeovo <845156436@qq.com>
2022-08-04 22:03:42 +0800

    修改api接口

--- vue.config.js
+++ vue.config.js
@@ -10,7 +10,7 @@ module.exports = defineConfig({
         // 例如将'localhost:8080/api/xxx'代理到'www.example.com/api/xxx'
         proxy: {
             '/api': { // 匹配所有以 '/api'开头的请求路径
-                target: 'http://129.150.63.35:8081', // 代理目标的基础路径
+                target: 'http://www.shaking.link:8088', // 代理目标的基础路径
                 // secure: false,  // 如果是https接口，需要配置这个参数
                 // changeOrigin: true, // 支持跨域
                 // ws: false,

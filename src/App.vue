b9f839fd5e04c7a76cbea060c4cad6e1ad285b67
likeorangeovo <845156436@qq.com>
2022-09-07 16:06:50 +0800

    修正

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
  * @Author: likeorange
  * @Date: 2022-06-18 13:28:13
  * @LastEditors: likeorange
- * @LastEditTime: 2022-09-07 12:37:05
+ * @LastEditTime: 2022-09-07 16:06:21
 -->
 <template>
   <div id="app">
@@ -36,7 +36,7 @@ export default {
   padding: 0;
 }
 #app {
-  background: url("http://shaking.link:888/images/background.png");
+  background: #b8e2d4;
   width: 100%;
   height: 100%;
   position: fixed;
